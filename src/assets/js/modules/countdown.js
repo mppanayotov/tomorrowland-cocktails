0c5e75d62563551f08304802610aed519593126e
mppanayotov <m.p.panayotov@gmail.com>
2023-05-18 20:19:20 +0300

    Update demo index page

--- src/assets/js/modules/countdown.js
+++ src/assets/js/modules/countdown.js
@@ -5,7 +5,7 @@
 $(window).on('load', function() {
 	function updateCountdown() {
 		const $countdown = $('#countdown');
-		const start = new Date('2020-07-25T15:00:00.000+01:00');
+		const start = new Date('2026-07-25T15:00:00.000+01:00');
 		const ts = countdown(start, null, countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
 
 		if (! $countdown.hasClass('is-initialized')) {
